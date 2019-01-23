939fe3781b6706374f4a6e6f12b8d6105aea2966
UDIRECT\jack.hoyle <jack.hoyle@udgroup.co.uk>
2019-01-23 13:18:16 +0000

    give TicketListItem some love: clickable new "Switch" option

--- app/components/TicketListItem.js
+++ app/components/TicketListItem.js
@@ -3,6 +3,7 @@ import classnames from 'classnames';
 import style from './TicketListItem.css';
 import * as TicketTypes from '../constants/TicketTypes';
 import * as TimeHelper from '../utils/TimeHelper';
+import * as Config from '../utils/Config';
 import moment from 'moment';
 import Modal from 'react-modal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -11,10 +12,12 @@ import onClickOutside from 'react-onclickoutside';
 import EditTicket from './EditTicket.js';
 import DeleteTicket from './DeleteTicket.js';
 
+
 class TicketListItem extends Component {
 
   static propTypes = {
-    ticket: PropTypes.object.isRequired
+    ticket: PropTypes.object.isRequired,
+    tickets: PropTypes.array.isRequired
   };
 
   constructor(props, context) {
@@ -52,16 +55,31 @@ class TicketListItem extends Component {
   handleDeleteClick = () => {
     this.setState({ deleteModalOpen: true, menuOpen: false });
   }
+  handleChangeClick = () => {
+    this.props.actions.completeCurrentTicket();
+    this.props.actions.addTicket(this.props.ticket.name, null, this.props.ticket.summary, this.props.ticket.type);
+    this.setState({ menuOpen: false });
+  }
+
+  handleClick = () => {
+    if (this.props.ticket.type != TicketTypes.BREAK)
+    {
+      Config.getBaseUrl().then(url => {
+        chrome.tabs.create({ url: 'http://' + url + '/browse/' + this.props.ticket.name });
+      });
+    }
+  }
 
   render() {
-    const { ticket } = this.props;
+    const { ticket, tickets } = this.props;
     return (
       <li style={{background: ticket.colour}} className={style.listItem}>
-        <span className={style.ticketDescription}>{ticket.name}{ticket.summary ? ' - ' : null}{ticket.summary}</span>
+        <span onClick={this.handleClick} className={style.ticketDescription}>{ticket.name}{ticket.summary ? ' - ' : null}{ticket.summary}</span>
         <div className={style.pushRight}>
           <label>{TimeHelper.FormatTime(ticket.duration)}</label>
           <FontAwesomeIcon icon={faEllipsisV} className={style.fa} onClick={this.toggleMenuVisible}/>
           <ul className={style.menu} style={{display: this.state.menuOpen ? 'block' : 'none' }}>
+            <li onClick={this.handleChangeClick}>Switch</li>
             <li onClick={this.handleEditClick}>Edit</li>
             <li onClick={this.handleDeleteClick}>Delete</li>
           </ul>
@@ -71,14 +89,14 @@ class TicketListItem extends Component {
             onRequestClose={this.closeEditModal}
             ariaHideApp={false}
         >
-          <EditTicket ticket={ticket} closeModal={this.closeEditModal} editTicket={this.props.EditTicket} duration={this.state.duration} />
+          <EditTicket ticket={ticket} tickets={tickets} closeModal={this.closeEditModal} updateProgress={this.props.actions.updateProgress} editTicket={this.props.actions.EditTicket} duration={this.state.duration} />
         </Modal>
         <Modal
             isOpen={this.state.deleteModalOpen}
             onRequestClose={this.closeDeleteModal}
             ariaHideApp={false}
         >
-          <DeleteTicket ticket={ticket} closeModal={this.closeDeleteModal} DeleteTicket={this.props.DeleteTicket} duration={this.state.duration} />
+          <DeleteTicket ticket={ticket} tickets={tickets} closeModal={this.closeDeleteModal} DeleteTicket={this.props.actions.DeleteTicket} duration={this.state.duration} />
         </Modal>
       </li>
     );
